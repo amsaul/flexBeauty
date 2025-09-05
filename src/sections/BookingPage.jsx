cab55de402745eafb2e22a9cdca522748a187eac
amsaul <amsaulbosire@gmail.com>
2025-09-05 19:28:10 +0300

    added booking page

--- src/sections/BookingPage.jsx
+++ src/sections/BookingPage.jsx
@@ -1,8 +1,22 @@
 import React from "react"
+import '../../src/assets/styles/booking.css'
 function BookingPage() {
     return(
-        <div> 
-            <p>this is the booking page</p>
+        <div className="booking-container"> 
+            <div className="bookingform">
+                <h2>book appointment</h2>
+                <form action="">
+                    <label> name </label>
+                    <input type="text" placeholder="enter your name" />
+                    <label> email </label>
+                    <input type="email" placeholder="enter email" />
+                    <label> phone number </label>
+                    <input type="text" placeholder="enter your phone number" />
+                    <label> message </label>
+                    <textarea type="text-area" placeholder="enter text" />
+                    <button>send</button>
+                </form>
+            </div>
         </div>
     )
 }
